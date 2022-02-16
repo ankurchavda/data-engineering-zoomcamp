e67463e4d5e62b8a5f2a9c1a3a28ce2da78b2af1
tremoloplusdelay <aachavda0789@gmail.com>
2022-02-16 18:55:32 +0530

    final flow

--- 4_dbt/dbt/taxi_rides_ny/models/staging/stg_green_tripdata.sql
+++ 4_dbt/dbt/taxi_rides_ny/models/staging/stg_green_tripdata.sql
@@ -1,5 +1,12 @@
 {{ config(materialized='view') }}
 
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
+  from {{ source('staging','green_tripdata') }}
+  where vendorid is not null 
+)
 select
     -- identifiers
     {{ dbt_utils.surrogate_key(['vendorid', 'lpep_pickup_datetime']) }} as tripid,
@@ -24,14 +31,15 @@ select
     cast(mta_tax as numeric) as mta_tax,
     cast(tip_amount as numeric) as tip_amount,
     cast(tolls_amount as numeric) as tolls_amount,
-    cast(ehail_fee as numeric) as ehail_fee,
+    cast(0 as numeric) as ehail_fee,
     cast(improvement_surcharge as numeric) as improvement_surcharge,
     cast(total_amount as numeric) as total_amount,
     cast(payment_type as integer) as payment_type,
     {{ get_payment_type_description('payment_type') }} as payment_type_description,
     cast(congestion_surcharge as numeric) as congestion_surcharge
-from {{source('staging','green_tripdata')}}
-where vendorid is not null
+from tripdata
+where rn = 1
+
 -- dbt build --m <model.sql> --var 'is_test_run: false'
 {% if var('is_test_run', default=true) %}
     limit 100
